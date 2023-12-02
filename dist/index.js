f30312295ed153582ce9d85523a94e039e924842
Gathsara2002 <liviru2002mmg@gmail.com>
2023-12-02 20:25:25 +0530

    Explicit type checked

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,22 @@
 "use strict";
 console.log("Hello world");
+//TS Types
+/*
+* string
+* number
+* boolean
+*bigint
+* undefine
+* null
+* any
+* unknown
+*  */
+//Declare variables
+//Explicit
+let myName = "Gathsara";
+let age = 21;
+let isStudent = true;
+let salary = 100000000;
+console.log(myName + " - " + age + " - " + isStudent + " - " + salary);
+//myName=12; //error
+//age="a";   //error
--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,27 @@
 console.log("Hello world");
+
+//TS Types
+
+/*
+* string
+* number
+* boolean
+*bigint
+* undefine
+* null
+* any
+* unknown
+*  */
+
+//Declare variables
+
+//Explicit
+
+let myName: string = "Gathsara";
+let age: number = 21;
+let isStudent: boolean = true;
+let salary: number = 100_000_000;
+console.log(myName + " - " + age + " - " + isStudent + " - " + salary);
+
+//myName=12; //error
+//age="a";   //error
